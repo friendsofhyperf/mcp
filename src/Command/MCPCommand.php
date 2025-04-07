3879591a41cd2d521e8e45d1980df62b0648fc43
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-04-07 23:11:31 +0800

    更新 MCPCommand 文件头信息以反映项目来源

--- src/Command/MCPCommand.php
+++ src/Command/MCPCommand.php
@@ -2,12 +2,11 @@
 
 declare(strict_types=1);
 /**
- * This file is part of Hyperf.
+ * This file is part of huangdijia/mcp-php-sdk.
  *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * @link     https://github.com/huangdijia/mcp-php-sdk
+ * @document https://github.com/huangdijia/mcp-php-sdk/blob/main/README.md
+ * @contact  Deeka Wong <huangdijia@gmail.com>
  */
 
 namespace FriendsOfHyperf\MCP\Command;
