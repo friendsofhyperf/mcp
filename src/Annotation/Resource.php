8643d8effa163a9b0815aeefb9712e5700f08d45
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-04-07 17:27:32 +0800

    重构 Resource 注解类，添加 schema 和 template 属性；优化 collectMethod 方法逻辑

--- src/Annotation/Resource.php
+++ src/Annotation/Resource.php
@@ -12,14 +12,36 @@ declare(strict_types=1);
 namespace FriendsOfHyperf\MCP\Annotation;
 
 use Attribute;
+use ModelContextProtocol\SDK\Shared\ResourceTemplate;
 
 #[Attribute(Attribute::TARGET_METHOD)]
 class Resource extends BaseAnnotation
 {
     public function __construct(
+        public string $schema = '',
+        public string $template = '',
         public string $name = '',
         public string $description = '',
+        public string $mimeType = 'text/plain',
         public string $server = '',
     ) {
+    }
+
+    public function collectMethod(string $className, ?string $target): void
+    {
+        $this->getServerManager()
+            ->getServer($this->server)
+            ->resource(
+                scheme: $this->schema,
+                template: new ResourceTemplate(
+                    template: $this->template,
+                    options: [
+                        'name' => $this->name,
+                        'description' => $this->description,
+                        'mimeType' => $this->mimeType,
+                    ],
+                ),
+                handler: [$this->getContainer()->get($className), $target],
+            );
     }
 }
